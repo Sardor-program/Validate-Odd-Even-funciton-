c43645e2dc966b31546fd86193650364d3139998
Sardor-program <sardorali378@gmail.com>
2023-10-21 23:28:32 +0500

    Create odd_and_even_FUNCTION.py

---
+++ odd_and_even_FUNCTION.py
@@ -0,0 +1,14 @@
+
+
+def isOdd(number):
+    if (number%2)==1:
+        return True
+    else:
+        return False
+
+
+def isEven(number):
+    if (number%2)==0:
+        return True
+    else:
+        return False
